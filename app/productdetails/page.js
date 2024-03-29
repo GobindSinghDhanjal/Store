56818a1877b7e122c0252901c1f9f93e3ca411cc
Gobind <gobindsinghdhanjal1@gmail.com>
2024-03-29 15:18:22 +0530

    resolved some errors

--- app/productdetails/page.js
+++ app/productdetails/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import {
   Box,
   CircularProgress,
@@ -37,59 +37,61 @@ const Page = () => {
 
   return (
     <div className="sub-container">
-      {product ? (
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Card sx={{ border: "none", boxShadow: "none" }}>
-              <CardMedia
-                component="img"
-                height="auto"
-                image={product.image}
-                alt={product.title}
-                style={{ objectFit: "contain", height: "280px" ,marginTop:"25px" }}
-              />
-            </Card>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box>
-              <Typography variant="h4" gutterBottom>
-                {product.title}
-              </Typography>
-              <Box display="flex" alignItems="center" marginBottom={2}>
-                <Rating
-                  name="rating"
-                  value={product.rating.rate}
-                  precision={0.5}
-                  readOnly
+      <Suspense fallback={<CircularProgress />}>
+        {product ? (
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Card sx={{ border: "none", boxShadow: "none" }}>
+                <CardMedia
+                  component="img"
+                  height="auto"
+                  image={product.image}
+                  alt={product.title}
+                  style={{ objectFit: "contain", height: "280px" ,marginTop:"25px" }}
                 />
-                <Typography>({product.rating.count} reviews)</Typography>
-              </Box>
-              <Typography mt={2} variant="body1" gutterBottom>
-                {product.description}
-              </Typography>
-              <Box display="flex" alignItems="center" marginBottom={2}>
-                <Typography variant="h6" marginRight={1}>
-                  Price:
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box>
+                <Typography variant="h4" gutterBottom>
+                  {product.title}
                 </Typography>
-                <Typography variant="h6">${product.price}</Typography>
-              </Box>
+                <Box display="flex" alignItems="center" marginBottom={2}>
+                  <Rating
+                    name="rating"
+                    value={product.rating.rate}
+                    precision={0.5}
+                    readOnly
+                  />
+                  <Typography>({product.rating.count} reviews)</Typography>
+                </Box>
+                <Typography mt={2} variant="body1" gutterBottom>
+                  {product.description}
+                </Typography>
+                <Box display="flex" alignItems="center" marginBottom={2}>
+                  <Typography variant="h6" marginRight={1}>
+                    Price:
+                  </Typography>
+                  <Typography variant="h6">${product.price}</Typography>
+                </Box>
 
-              <Button variant="contained" color="primary" size="large">
-                Add to Cart
-              </Button>
-            </Box>
+                <Button variant="contained" color="primary" size="large">
+                  Add to Cart
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
-      ) : (
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="300px"
-        >
-          <CircularProgress />
-        </Box>
-      )}
+        ) : (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="300px"
+          >
+            <CircularProgress />
+          </Box>
+        )}
+      </Suspense>
     </div>
   );
 };
